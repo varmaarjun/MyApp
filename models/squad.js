bde216ebcced0767f2153d862b8ee22a31636ce8
Arjun Varma <89406788+varmaarjun@users.noreply.github.com>
2025-03-15 04:52:44 +0530

    Delete models directory

--- models/squad.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const squadSchema = new mongoose.Schema({
-  team: String,
-  playerName: String
-});
-
-const Squad = mongoose.model('Squad', squadSchema);
-module.exports = Squad;
