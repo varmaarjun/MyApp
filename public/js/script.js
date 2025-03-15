04ae8d95f664168c362dd0116a23be9945e8ed4f
Arjun <varma.arjun09@gmail.com>
2025-03-15 18:50:56 +0530

    update

--- public/js/script.js
+++ public/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
 
-            const response = await fetch('/.netlify/functions/auth', {
+            const response = await fetch('/.netlify/functions/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password })
